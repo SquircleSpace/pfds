e799d2e385b7bd53aa6536e862b58b1c6040f407
Ada Avery <2561504+SquircleSpace@users.noreply.github.com>
2019-11-22 01:08:39 -0800

    Factor out readonly struct logic from ADT logic

--- common.lisp
+++ common.lisp
@@ -18,7 +18,8 @@
    #:compare #:define-type-id #:compare*)
   (:export
    #:is-empty #:empty #:with-member #:without-member #:is-member
-   #:define-interface #:compare #:compare* #:define-type-id))
+   #:define-interface #:compare #:compare* #:define-type-id
+   #:define-structure))
 (in-package :pfds.shcl.io/common)
 
 (defgeneric is-empty (container))
@@ -41,26 +42,34 @@
      ',name))
 
 (defmacro define-adt (base-name common-slots &body subtypes)
+  (unless subtypes
+    (warn "An ADT without subtypes cannot be initialized"))
   (labels
       ((handle-subtype (definition)
          (destructuring-bind (name &rest slots) definition
            (when (symbolp name)
              (setf name `(,name)))
            (destructuring-bind (real-name &rest options) name
-             (setf name `(,real-name (:include ,base-name) ,@options))
-             `(progn
-                (defstruct ,name ,@(mapcar #'handle-slot slots))
-                (define-type-id ,real-name)))))
-       (handle-slot (slot-definition)
+             (setf name `(,real-name (:include ,base-name) ,@options)))
+           `(define-structure ,name ,@slots))))
+    `(progn
+       (define-structure (,base-name (:constructor ,(gensym (symbol-name base-name))))
+         ,@common-slots)
+       ,@(mapcar #'handle-subtype subtypes))))
+
+(defmacro define-structure (name-and-options &body slots)
+  (labels
+      ((handle-slot (slot-definition)
          (etypecase slot-definition
            (symbol
             `(,slot-definition nil :read-only t))
            (cons
-            (destructuring-bind (name default-value &rest options) slot-definition
+            (destructuring-bind (name &optional default-value &rest options) slot-definition
               `(,name ,default-value :read-only t ,@options))))))
-    `(progn
-       (progn
-         (defstruct (,base-name (:constructor ,(gensym (symbol-name base-name))))
-           ,@(mapcar #'handle-slot common-slots))
-         (define-type-id ,base-name))
-       ,@(mapcar #'handle-subtype subtypes))))
+    (let ((name (etypecase name-and-options
+                  (list (car name-and-options))
+                  (symbol name-and-options))))
+      `(progn
+         (defstruct ,name-and-options
+           ,@(mapcar #'handle-slot slots))
+         (define-type-id ,name)))))
