ade328fcde726759fe568f53361288657b63b57a
Ada Avery <2561504+SquircleSpace@users.noreply.github.com>
2019-12-05 18:34:44 -0800

    Hijack the copier for immutable structures
    This isn't how I'd like it to work long-term.  I'd prefer to have a
    proper metaclass-like setup where you declare the metaclass of the
    struct and that influences how things are generated.  E.g. a
    IMMUTABLE-STRUCTURE metaclass might generate this sort of copier
    rather than the default.  But... this little diversion has gone on
    long enough.  Its time to actually leverage the auto-generated copier.

--- common.lisp
+++ common.lisp
@@ -16,7 +16,10 @@
   (:use :common-lisp)
   (:import-from :pfds.shcl.io/compare
    #:compare #:define-type-id #:compare* #:compare-objects)
-  (:import-from :pfds.shcl.io/structure-mop #:register-structure)
+  (:import-from :pfds.shcl.io/structure-mop
+   #:register-structure #:find-structure-metaobject
+   #:structure-metaobject-constructors #:structure-metaobject-effective-slots
+   #:effective-slot-name #:effective-slot-readers)
   (:export
    #:is-empty #:empty #:with-member
    #:define-interface #:compare #:compare* #:define-type-id
@@ -62,19 +65,75 @@
          ,@(mapcar #'handle-subtype subtypes)))))
 
 (defmacro define-structure (name-and-options &body slots)
-  (labels
-      ((handle-slot (slot-definition)
-         (etypecase slot-definition
-           (symbol
-            `(,slot-definition nil :read-only t))
-           (cons
-            (destructuring-bind (name &optional default-value &rest options) slot-definition
-              `(,name ,default-value :read-only t ,@options))))))
-    (let ((name (etypecase name-and-options
-                  (list (car name-and-options))
-                  (symbol name-and-options))))
-      `(progn
-         (defstruct ,name-and-options
-           ,@(mapcar #'handle-slot slots))
-         (register-structure ,name-and-options ,@slots)
-         (define-type-id ,name)))))
+  (when (symbolp name-and-options)
+    (setf name-and-options (list name-and-options)))
+  (destructuring-bind (name &rest options) name-and-options
+    (labels
+        ((handle-slot (slot-definition)
+           (etypecase slot-definition
+             (symbol
+              `(,slot-definition nil :read-only t))
+             (cons
+              (destructuring-bind (name &optional default-value &rest options) slot-definition
+                `(,name ,default-value :read-only t ,@options)))))
+         (default-copier-name ()
+           (intern (format nil "COPY-~A" (symbol-name name))))
+         (copier-option-p (option)
+           (or (eq option :copier)
+               (and (consp option)
+                    (eq (car option) :copier)))))
+      (let (copier
+            copier-set)
+        (dolist (option options)
+          (when (copier-option-p option)
+            (when copier-set
+              (error ":copier may only be specified once"))
+            (setf copier-set t)
+            (setf copier (if (or (eq option :copier)
+                                 (null (cdr option)))
+                             (default-copier-name)
+                             (second option)))))
+
+        (unless copier-set
+          (setf copier (default-copier-name)))
+
+        (setf options (cons '(:copier nil) (remove-if #'copier-option-p options)))
+        `(progn
+           (defstruct (,name ,@options)
+             ,@(mapcar #'handle-slot slots))
+           (register-structure (,name ,@options) ,@slots)
+           ,@(when copier
+               `((define-changer-function ,copier ,name)))
+           (define-type-id ,name)
+           ',name)))))
+
+(defmacro define-changer-function (name structure-name)
+  (let* ((metaobject (or (find-structure-metaobject structure-name)
+                         (error "couldn't lookup structure")))
+         (constructor (or (loop :for potential :in (structure-metaobject-constructors metaobject)
+                                :do (when (symbolp potential)
+                                      (return potential)))
+                          (error "couldn't find an eligible constructor")))
+         (slots (structure-metaobject-effective-slots metaobject))
+         (slot-names (mapcar 'effective-slot-name slots))
+         (slot-readers (mapcar (lambda (s) (car (effective-slot-readers s))) slots))
+         (slot-vars (mapcar 'gensym slot-names))
+         (slot-keywords (mapcar (lambda (name) (intern name :keyword)) slot-names))
+         (object (make-symbol "OBJECT")) ;; so it doesn't get an ugly number
+         (key-args (mapcar (lambda (keyword var reader)
+                             `((,keyword ,var) (,reader ,object)))
+                           slot-keywords slot-vars slot-readers)))
+    (unless slots
+      (return-from define-changer-function
+        `(defun ,name (,object)
+           ,object)))
+
+    `(defun ,name (,object &key ,@key-args)
+       (if (and ,@(mapcar (lambda (var reader)
+                            `(eql ,var (,reader ,object)))
+                          slot-vars slot-readers))
+           ,object
+           (,constructor
+            ,@(mapcan (lambda (keyword var)
+                        `(,keyword ,var))
+                      slot-keywords slot-vars))))))
