1c896ac44465375d148d5f299ec0b00191a6b733
Ada Avery <2561504+SquircleSpace@users.noreply.github.com>
2020-02-08 18:53:25 -0800

    Rename the list implementation file

--- implementation/pure-list.lisp
+++ implementation/pure-list.lisp
@@ -12,7 +12,7 @@
 ;; See the License for the specific language governing permissions and
 ;; limitations under the License.
 
-(defpackage :pfds.shcl.io/implementation/list
+(defpackage :pfds.shcl.io/implementation/pure-list
   (:use :common-lisp)
   (:import-from :pfds.shcl.io/interface/common
    #:to-list)
@@ -25,7 +25,7 @@
   (:export
    #:with-head #:head #:tail #:is-empty #:empty #:empty-pure-list
    #:pure-list #:pure-list* #:pure-list-cons))
-(in-package :pfds.shcl.io/implementation/list)
+(in-package :pfds.shcl.io/implementation/pure-list)
 
 ;; See "Purely Functional Data Structures" by Chris Okasaki
 
